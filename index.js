0af5ac08e9d4ac109ec457f2ea8670d6bc6b9309
Adedokun Adeshina <adeshina93@gmail.com>
2025-01-30 04:53:56 -0800

    backend

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const app =express();
-app.get("/", (req, res)=>{
-    res.send("Hello World");
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    const current_datetime = new Date();
+    res.send({
+        email: process.env.EMAIL,
+        current_datetime: current_datetime.toISOString(),
+        github_url: "",
+    });
 });
 
-
-const PORT = 3000; 
-app.listen(PORT, ()=>{
-    console.log(`"Server is running at port:" ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
